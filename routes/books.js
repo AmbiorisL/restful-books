60450d30576643324e0893bf9a1a050c0ba5cd96
AmbiorisL <Ambioris_Lora@tjx.com>
2022-03-02 11:08:59 -0500

    fixed merge issues

--- routes/books.js
+++ routes/books.js
@@ -2,10 +2,26 @@
 var express = require('express');
 var router = express.Router();
 
-// get  /books/
+const books = [
+  {
+    id: '1',
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    authorId: '1',
+    year: 1925,
+  },
+  {
+    id: '2',
+    title: 'Clifford, the Big Red Dog',
+    author: 'Norman Bridwell',
+    authorId: '2',
+    year: 1963,
+  },
+];
 
+// GET /books/ -> [ array of books ]
 router.get('/', (req, res) => {
-  res.json('this should return books');
+  res.json(books);
 });
 
 router.get('/gatsby', (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
